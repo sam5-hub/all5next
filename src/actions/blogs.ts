1768ef9fd840881348dc3a6b3836603bb6f63402
Sam Wuu <iamkeason@gmail.com>
2024-06-15 00:47:25 +0800

    init 2

--- src/actions/blogs.ts
+++ src/actions/blogs.ts
@@ -22,6 +22,45 @@ export const onGetBlogList = async (page: number, perPage: number) => {
       // },
     });
 
+
+    if (data) {
+    
+      return {
+        status: 200,
+        data,
+        total,
+        perPage,
+        currentPage: page,
+      };
+    }
+  } catch (error) {
+    console.log(error);
+    return {
+      status: 500,
+      message: 'Internal server error',
+    };
+  }
+};
+
+export const onGetBlogList = async (page: number, perPage: number) => {
+  try {
+    const userId = 1;
+
+    // Calculate the number of records to skip
+    const skip = (page - 1) * perPage;
+
+    // Fetch the paginated data
+    const data = await prisma.blogs.findMany({
+      skip,
+      take: perPage,
+      orderBy: {
+        blog_id: 'desc',
+      }
+      // where: {
+      //   userId, // assuming you want to filter blogs by userId
+      // },
+    });
+
     // Fetch the total count of blogs
     const total = await prisma.blogs.count({
       // where: {
